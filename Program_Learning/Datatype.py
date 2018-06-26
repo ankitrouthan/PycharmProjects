e3a3d2dc14303e752aa2a1802adedc026b09ac76
LAPTOP-S6KOE961\LUCKY-LP <ankitrouthan2@gmail.com>
2018-06-26 23:21:04 +0530

    Initial commit

---
+++ Program_Learning/Datatype.py
@@ -0,0 +1,17 @@
+sample='''Hi,My name is ankit 
+i have done my Btech from hbti kanpur
+in the year 2014'''
+
+
+print(sample + " i joined cognizant in the year 2015")
+
+
+string1='Routhan'
+print(string1[2:6])
+print(string1[-6])
+
+
+print(sample.count('H',0,9))
+
+print(sample.isalpha())
+print(sample.upper())
---
+++ Program_Learning/airthmetic.py
@@ -0,0 +1,32 @@
+num1=10
+num2=20
+print("num1 + num2=",num1+num2)
+print("num1 + num2=",num1-num2)
+print("num1 + num2=",num1*num2)
+print("num1 + num2=",num1/num2)
+
+
+'''
+#Comparison
+print("is num1 > num2",num1 > num2)
+print("is num1 = num2",num1 == num2)
+print("is num1 != num2",num1 != num2)
+'''
+
+#Logical
+'''
+x=True
+y=False
+
+print("x and y", x and y)
+print("x or y", x or y)
+print("not of x", not x)
+'''
+
+#Bitwise
+num4=3
+num5=6
+
+print("bitwise OR=", num4 | num5)
+print("bitwise XOR=",num4 ^ num5)
+
